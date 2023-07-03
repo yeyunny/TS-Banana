81826bc610b71eb94a7aa40625ab5023ddc8eb83
yeyun <hallo.yunye@gmail.com>
2023-07-03 23:10:43 +0900

    [ADD] react 기초세팅

---
+++ src/components/FeedContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FeedContainer() {
+  return (
+    <div>
+      <h1>Feed</h1>
+    </div>
+  );
+}
+
+export default FeedContainer;
---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/Nav";
+import FeedContainer from "../components/FeedContainer";
+
+function Home() {
+  return (
+    <div>
+      <Nav />
+      <FeedContainer />
+    </div>
+  );
+}
+
+export default Home;
