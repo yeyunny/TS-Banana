fe3dd722074522c54417a2a3e7ab8d0984dccad5
yeyun <hallo.yunye@gmail.com>
2023-07-04 20:12:13 +0900

    [ADD] localStorage get / set item

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,22 +6,21 @@ function Title() {
   const inputHandler = (event) => {
     setUsername(event.target.value);
   };
-  console.log(username);
 
   const submitHandler = () => {
     localStorage.setItem("username", username);
   };
+  console.log(username);
 
   const nickname = localStorage.getItem("username");
-
   return (
     <div>
       <h1>⚡️바나나 레전드짤 모음집🍌</h1>
       {nickname ? (
         <span>{nickname}</span>
       ) : (
-        <form id="login-form">
-          <input onChange={inputHandler} placeholder="Who Ard You?"></input>
+        <form>
+          <input onChange={inputHandler} placeholder="Who Are You?"></input>
           <button onClick={submitHandler}>Enter</button>
         </form>
       )}
