03334894b1e02683c59875d0cf89a0c3643f40e3
yeyun <hallo.yunye@gmail.com>
2023-07-08 00:30:08 +0900

    [ADD] prev, 익명함수, value={comments}

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,26 +1,40 @@
 import { useState } from "react";
 import "./Feeds.scss";
 
-let commentsList = [];
-
 function Feeds({ url }) {
-  const [comments, setComments] = useState([]);
+  const [comments, setComments] = useState("");
+  const [commentsList, setCommentsList] = useState([]);
 
   const inputHandler = (event) => {
     setComments(event.target.value);
   };
 
   const submitHandler = () => {
-    commentsList.push(comments);
+    setCommentsList((prev) => [...prev, comments]);
+    setComments("");
+  };
 
-    console.log("안안", commentsList);
+  const deleteHandler = (comment) => {
+    const list = commentsList.filter((item) => item !== comment);
+    setCommentsList(list);
   };
 
   return (
     <div>
       <img src={url} alt="NANA" />
+      <div>
+        {commentsList.map((comment, index) => {
+          return (
+            <div>
+              <span key={index}>{comment}</span>
+              <button onClick={() => deleteHandler(comment)}>✖️</button>
+            </div>
+          );
+        })}
+      </div>
       <div className="comments">
         <input
+          value={comments}
           onChange={inputHandler}
           type="text"
           placeholder="귀여운 나나에게 한 마디 !"
