aafa5a59f68a00e7e1fa6a56f9907a71c87db2ea
yeyun <hallo.yunye@gmail.com>
2023-07-07 22:23:58 +0900

    [ADD] Feeds.jsx input, submit Handler 추가

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import "./Feeds.scss";
 
+let commentsList = [];
+
 function Feeds({ url }) {
+  const [comments, setComments] = useState([]);
+
+  const inputHandler = (event) => {
+    setComments(event.target.value);
+  };
+
+  const submitHandler = () => {
+    commentsList.push(comments);
+
+    console.log("안안", commentsList);
+  };
+
   return (
     <div>
       <img src={url} alt="NANA" />
+      <div></div>
       <div className="comments">
-        <input type="text" placeholder="귀여운 나나에게 한 마디 !"></input>
-        <button>Enter</button>
+        <input
+          onChange={inputHandler}
+          type="text"
+          placeholder="귀여운 나나에게 한 마디 !"
+        ></input>
+        <button onClick={submitHandler}>Enter</button>
       </div>
     </div>
   );
