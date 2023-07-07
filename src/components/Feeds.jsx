c20f0c03ffcb0c982161cffcfa89cf59d8619d93
yeyun <hallo.yunye@gmail.com>
2023-07-07 19:39:38 +0900

    [ADD] common.css 관련 정보

--- src/components/Feeds.jsx
+++ src/components/Feeds.jsx
@@ -4,13 +4,9 @@ function Feeds({ url }) {
   return (
     <div>
       <img src={url} alt="NANA" />
-      <div>
-        <input
-          id="feedInput"
-          type="text"
-          placeholder="귀여운 나나에게 한 마디 !"
-        ></input>
-        <button id="feedButton">Enter</button>
+      <div className="comments">
+        <input type="text" placeholder="귀여운 나나에게 한 마디 !"></input>
+        <button>Enter</button>
       </div>
     </div>
   );
